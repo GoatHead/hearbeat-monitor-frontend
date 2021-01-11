471db5dc2b1df4ce5823211b42e37ded5e97d88a
erpsm92 <erpsm92@gmail.com>
2021-01-11 19:04:17 +0900

    타임존을 제대로 못잡는 문제 수정

--- src/utils/moment-util.ts
+++ src/utils/moment-util.ts
@@ -2,5 +2,6 @@ import {MomentFormat} from "../enums/moment-format";
 import moment from 'moment'
 
 export function format(datetime: string, format: MomentFormat): string {
-    return moment(datetime).format(format)
+    console.log(datetime)
+    return moment(datetime).utc().format(format)
 }
